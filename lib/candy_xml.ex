996ca1c638a965a64202c8fa6c0fb78512f6c5d3
Vikram Ramakrishnan <lord.ezar@gmail.com>
2016-02-18 20:55:55 -0500

    add parse function

--- lib/candy_xml.ex
+++ lib/candy_xml.ex
@@ -1,7 +1,55 @@
 defmodule CandyXml do
   import SweetXml
 
-  def parse!(xml) do
-    # pass an xml to this which returns structs
+  defstruct [:title, :link, :summary, :updated_date, :rss_feed_id, :cik_id]
+
+  def parse(xml) do
+    %CandyXml{
+      title: parse_title(xml),
+      link: parse_link(xml),
+      summary: parse_summary(xml),
+      updated_date: parse_updated_date(xml),
+      rss_feed_id: parse_rss_feed_id(xml),
+      cik_id: parse_cik_id(xml)
+    }
+  end
+
+  defp parse_title(xml) do
+    xml
+    |> xpath(~x"./title/text()"s)
+  end
+
+  defp parse_link(xml) do
+    xml
+    |> xpath(~x"./link/text()"s)
+  end
+
+  defp parse_summary(xml) do
+    xml
+    |> xpath(~x"./summary/text()"s)
+  end
+
+  defp parse_updated_date(xml) do
+    xml
+    |> xpath(~x"./updated/text()"s)
+  end
+
+  defp parse_rss_feed_id(xml) do
+    xml
+    |> xpath(~x"./id/text()"s)
+  end
+
+  defp parse_cik_id(xml) do
+    regex = ~r/\(\d+\)/
+    title = xml
+    |> xpath(~x"./title/text()"s)
+
+    Regex.run(regex, title, capture: :first)
+    |> hd
+    |> String.split("(")
+    |> tl
+    |> hd
+    |> String.split(")")
+    |> hd
   end
 end
--- test/candy_xml_test.exs
+++ test/candy_xml_test.exs
@@ -1,6 +1,6 @@
 defmodule CandyXmlTest do
   use ExUnit.Case
-  doctest CandyXml
+
   import CandyXml
 
   def entry_xml do
@@ -18,50 +18,48 @@ defmodule CandyXmlTest do
     """
   end
 
-  # https://www.sec.gov/cgi-bin/browse-edgar?action=getcurrent&type=10-K&company=&dateb=&owner=include&start=0&count=100&output=atom
+  #test "parse!/1: parses RSS atom compliant XML" do
+    #xml = File.read!("test/fixtures/filings_atom_feed.xml")
+    #assert parse!(xml)
+  #end
 
-  test "parse!/1: parses RSS atom compliant XML" do
-    xml = Fiile.read!("test/fixtures/filings_atom_feed.xml")
-    assert parse!(xml)
-  end
+  #test "parse!/1: raises error on invalid XML" do
+    #xml = "naw"
+    #assert_raise RuntimeError, fn -> parse!(xml) end
+  #end
 
-  test "parse!/1: raises error on invalid XML" do
-    xml = "naw"
-    assert_raise RuntimeError, fn -> parse!(xml) end
-  end
-
-  test "has an entry" do
-    {:ok, feed} = File.read("test/fixtures/filings_atom_feed.xml") |> parse
-    assert feed.entry
-  end
+  #test "has an entry" do
+    #{:ok, feed} = File.read("test/fixtures/filings_atom_feed.xml") |> parse
+    #assert feed.entry
+  #end
 
   test "parses cik_id" do
-    {:ok, feed} = entry_xml |> parse
-    assert.cik_id == '0001121788'
+    entry = entry_xml |> parse
+    assert entry.cik_id == "0001121788"
   end
 
   test "parses link" do
-    {:ok, feed} = entry_xml |> parse
-    assert.link == "http://www.sec.gov/Archives/edgar/data/1121788/000161577416004243/0001615774-16-004243-index.htm"
+    entry = entry_xml |> parse
+    assert entry.link == "http://www.sec.gov/Archives/edgar/data/1121788/000161577416004243/0001615774-16-004243-index.htm"
   end
 
   test "parses rss_feed_id" do
-    {:ok, feed} = entry_xml |> parse
-    assert.rss_feed_id = "urn:tag:sec.gov,2008:accession-number=0001615774-16-004243"
+    entry = entry_xml |> parse
+    assert entry.rss_feed_id == "urn:tag:sec.gov,2008:accession-number=0001615774-16-004243"
   end
 
   test "parses summary" do
-    {:ok, feed} = entry_xml |> parse
-    assert.summary = "&lt;b&gt;Filed:&lt;/b&gt; 2016-02-17 &lt;b&gt;AccNo:&lt;/b&gt; 0001615774-16-004243 &lt;b&gt;Size:&lt;/b&gt; 8 MB"
+    entry = entry_xml |> parse
+    assert entry.summary == "&lt;b&gt;Filed:&lt;/b&gt; 2016-02-17 &lt;b&gt;AccNo:&lt;/b&gt; 0001615774-16-004243 &lt;b&gt;Size:&lt;/b&gt; 8 MB"
   end
 
   test "parses title" do
-    {:ok, feed} = entry_xml |> parse
-    assert.title = "10-K - GARMIN LTD (0001121788) (Filer)"
+    entry = entry_xml |> parse
+    assert entry.title == "10-K - GARMIN LTD (0001121788) (Filer)"
   end
 
   test "parses updated_date" do
-    {:ok, feed} = entry_xml |> parse
-    assert.updated_date = "2016-02-17T17:24:49-05:00"
+    entry = entry_xml |> parse
+    assert entry.updated_date == "2016-02-17T17:24:49-05:00"
   end
 end
